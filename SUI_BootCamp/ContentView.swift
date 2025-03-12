06dfda538ff5fa6a003af2356a870dd3f03ae279
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-12 22:27:38 +0100

    Initial Commit

---
+++ SUI_BootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SUI_BootCamp
+//
+//  Created by Miguel Gallego on 12/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SUI_BootCamp/SUI_BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_BootCampApp.swift
+//  SUI_BootCamp
+//
+//  Created by Miguel Gallego on 12/3/25.
+//
+
+import SwiftUI
+
+@main
+struct SUI_BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
