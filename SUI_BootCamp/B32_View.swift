5723cafcb212e7b9e69ced38bc92d5aa2336b4a8
Miguel Gallego Martín <miguel.0.1.1.2.3.5.8@gmail.com>
2025-03-12 22:46:23 +0100

    B32 done

--- SUI_BootCamp/B32_View.swift
+++ SUI_BootCamp/B32_View.swift
@@ -6,16 +6,40 @@ import SwiftUI
 
 struct B32_View: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .center, spacing: 10){
+            Text("First Line")
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .border(.red)
+                .frame(minHeight: 100)
+                .border(.blue)
+                .font(.title3)
+                .foregroundStyle(.cyan)
+            Text("Second Line")
+                .border(.red)
+                .foregroundStyle(.green)
+            Text("Third Line")
+                .border(.red)
+            HStack {
+                Text("Left")
+                Text("Right")
+                    .fontWeight(.heavy)
+            }
         }
-        .padding()
+        .border(.red)
+        .foregroundStyle(.orange)
     }
 }
 
 #Preview {
     B32_View()
 }
+
+/*
+ Topics:  
+ 
+ VStack  HStack  spacing
+ .foregroundStyle
+ .font
+ .fontWeight
+ 
+ */
